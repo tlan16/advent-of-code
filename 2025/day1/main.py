f5f16452f381390ff106b351ece9eddb7b6f5ef0
Frank Lan <franklan118@gmail.com>
2025-12-02 19:22:57 +1100

    day 1

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -1,23 +1,45 @@
 import re
 from pathlib import Path
-from typing import Generator
+from typing import Generator, Literal
+import pydantic
 
-input_file = Path(__file__)
+input_file = Path(__file__).parent / 'input.txt'
+test_input_file = Path(__file__).parent / 'test_input.txt'
 
 valid_line_pattern = re.compile(r"^[L|R]\d+$")
 
-def read_input() -> Generator[str]:
-    with open('input.txt', 'r') as file:
+class Line(pydantic.BaseModel):
+    direction: Literal["L", "R"]
+    offset: int
+
+
+def read_input(file: Path) -> Generator[Line]:
+    with open(file, 'r') as file:
         for line in file:
             line = line.strip()
             if len(line) > 0:
                 assert valid_line_pattern.match(line), f"Invalid line format: {line}"
-                yield line
+                # noinspection PyTypeChecker
+                yield Line(direction=line[0], offset=int(line[1:]))
+
+while_size = 100
+def rotate(initial_position: int, line: Line) -> int:
+    if line.direction == "L":
+        return (initial_position - line.offset) % while_size
+    else:
+        return (initial_position + line.offset) % while_size
+
 
 def main() -> None:
-    for line in read_input():
+    cursor: int = 50
+    counter: int = 0
+    for line in read_input(input_file):
+        print(f"cursor={cursor} counter={counter}")
         print(line)
-
+        cursor = rotate(cursor, line)
+        if cursor == 0:
+            counter += 1
+    print(f"cursor={cursor} counter={counter} [FINAL]")
 
 if __name__ == "__main__":
     main()
